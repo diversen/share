13685ddd2775aa9231b6287b5e58f0d205ac2a3c
Dennis Iversen <dennis.iversen@gmail.com>
2014-07-04 17:11:32 +0200

    minor

--- module.php
+++ module.php
@@ -1,11 +1,33 @@
 <?php
 
 class share {
+    /**
+     * automatic subModulePostContent when module is enabled as sub module
+     * in your share template all values should be rawurlencoded
+     * @param array $options ('return_url' => 'http://return.com', 
+     *                        'entity' => array ('title' => 'title', 'abstract' => 'abstract')
+     *                       ); 
+     * 
+     * 
+     * 
+     * @return strin $share string
+     */
     public static function subModulePostContent($options) {       
         $template = config::getModuleIni('share_template');
         $str = view::get('share', $template, $options);
         return $str;
     }
     
+    /**
+     * get a template for share. 
+     * in your share template all values should be rawurlencoded
+     * @param array $options ('return_url' => 'http://return.com', 
+     *                        'entity' => array ('title' => 'title', 'abstract' => 'abstract')
+     *                       ); 
+     * @return string $str
+     */
+    public static function get ($template, $options) {
+        $str = view::get('share', $template, $options);
+        return $str;
+    }  
 }
-
