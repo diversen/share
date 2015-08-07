4228f4520b62ce67dbcd0e8171096a9c9e7024d2
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:57:51 +0200

    lang

---
+++ lang/en/language.php
@@ -0,0 +1,15 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/share/views/stackoverflow.inc
+
+$LANG['share_stack_text'] = 'share_stack_text';
+// Translation of file modules/share/system_lang.inc
+
+$LANG['share_config_description'] = 'share_config_description';
+$LANG['/share/blocks/github_share.inc-human'] = '/share/blocks/github_share.inc-human';
+$LANG['/share/blocks/facebook_likebox.inc-human'] = '/share/blocks/facebook_likebox.inc-human';
+$LANG['/share/blocks/facebook_likebox_js.inc-human'] = '/share/blocks/facebook_likebox_js.inc-human';
+$LANG['/share/blocks/google_plusone.inc-human'] = '/share/blocks/google_plusone.inc-human';
+$LANG['/share/blocks/facebook_share.inc-human'] = '/share/blocks/facebook_share.inc-human';
