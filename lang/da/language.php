cbfc05c70cb93e44a2bc105ac1a489a1bfc25488
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:20:39 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,11 @@
+<?php
+
+$LANG = array();
+
+$LANG['Share'] = 'Del';
+$LANG['share_config_description'] = 'share_config_description';
+$LANG['/share/blocks/github_share.inc-human'] = 'github.com stjerner';
+$LANG['/share/blocks/facebook_likebox.inc-human'] = 'facebook.com box';
+$LANG['/share/blocks/facebook_likebox_js.inc-human'] = 'facebook.com likebox';
+$LANG['/share/blocks/google_plusone.inc-human'] = 'Google ';
+$LANG['/share/blocks/facebook_share.inc-human'] = 'Facebook Del knap';
--- lang/en/language.php
+++ lang/en/language.php
@@ -4,12 +4,12 @@ $LANG = array();
 
 // Translation of file modules/share/views/stackoverflow.inc
 
-$LANG['share_stack_text'] = 'share_stack_text';
+$LANG['Share'] = 'Share';
 // Translation of file modules/share/system_lang.inc
 
 $LANG['share_config_description'] = 'share_config_description';
-$LANG['/share/blocks/github_share.inc-human'] = '/share/blocks/github_share.inc-human';
-$LANG['/share/blocks/facebook_likebox.inc-human'] = '/share/blocks/facebook_likebox.inc-human';
-$LANG['/share/blocks/facebook_likebox_js.inc-human'] = '/share/blocks/facebook_likebox_js.inc-human';
-$LANG['/share/blocks/google_plusone.inc-human'] = '/share/blocks/google_plusone.inc-human';
-$LANG['/share/blocks/facebook_share.inc-human'] = '/share/blocks/facebook_share.inc-human';
+$LANG['/share/blocks/github_share.inc-human'] = 'github.com stars';
+$LANG['/share/blocks/facebook_likebox.inc-human'] = 'facebook.com box';
+$LANG['/share/blocks/facebook_likebox_js.inc-human'] = 'facebook.com likebox';
+$LANG['/share/blocks/google_plusone.inc-human'] = 'Google+';
+$LANG['/share/blocks/facebook_share.inc-human'] = 'facebook share button';
